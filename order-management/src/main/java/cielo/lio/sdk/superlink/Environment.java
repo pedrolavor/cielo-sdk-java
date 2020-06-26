23c20cb6488b44130d9062ecc1f09161282a3231
Pedro Lavôr <thiagolavor33@gmail.com>
2020-06-26 16:38:17 -0400

    adicionando metodos do superlink

--- order-management/src/main/java/cielo/lio/sdk/superlink/Environment.java
+++ order-management/src/main/java/cielo/lio/sdk/superlink/Environment.java
@@ -1,29 +1,36 @@
 package cielo.lio.sdk.superlink;
 
+import lombok.Getter;
+
 /**
  * API Environment URLs
  * <p/>
  * <li>{@link #PRODUCTION}</li>
  * <li>{@link #SANDBOX}</li>
  */
-public enum Environment implements cielo.lio.sdk.Environment {
+@Getter
+public enum Environment {
 	/**
 	 * Environment.PRODUCTION
 	 */
-	PRODUCTION("https://cieloecommerce.cielo.com.br/api/public/v1"),
+	PRODUCTION(
+		"https://cieloecommerce.cielo.com.br/api/public/v2",
+		"https://cieloecommerce.cielo.com.br/api/public/v1"
+	),
 
 	/**
 	 * Environment.SANDBOX
 	 */
-	SANDBOX("https://cieloecommerce.cielo.com.br/sandbox/api/public/v1");
+	SANDBOX(
+		"https://cieloecommerce.cielo.com.br/sandbox/api/public/v2",
+		"https://cieloecommerce.cielo.com.br/sandbox/api/public/v1"
+	);
 
-	private final String url;
+	private final String accessTokenUrl;
+	private final String resourceUrl;
 
-	Environment(String url) {
-		this.url = url;
-	}
-
-	public String getUrl() {
-		return this.url;
+	Environment(String accessTokenUrl, String resourceUrl) {
+		this.accessTokenUrl = accessTokenUrl;
+		this.resourceUrl = resourceUrl;
 	}
 }
--- order-management/src/main/java/cielo/lio/sdk/superlink/SuperLink.java
+++ order-management/src/main/java/cielo/lio/sdk/superlink/SuperLink.java
@@ -4,33 +4,69 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
 
-import cielo.lio.sdk.superlink.Product.Type;
-
 public class SuperLink {
 
   private OAuth2RestTemplate restTemplate;
-  private cielo.lio.sdk.Environment environment;
+  private Environment environment;
 
-  public SuperLink() { 
+  public SuperLink(String clientId, String clientSecret) { 
+    this.environment = Environment.PRODUCTION;
+
 		ClientCredentialsResourceDetails resourceDetails = new ClientCredentialsResourceDetails();
-		resourceDetails.setAccessTokenUri("http://localhost:8081/oauth/token");
-		resourceDetails.setClientId("jafoi-dev");
-		resourceDetails.setClientSecret("jafoi-development-cs-1027");
+		resourceDetails.setAccessTokenUri(this.environment.getAccessTokenUrl() + "/token");
+		resourceDetails.setClientId(clientId);
+		resourceDetails.setClientSecret(clientSecret);
     
     this.restTemplate = new OAuth2RestTemplate(resourceDetails);
+  }
+
+  public SuperLink(String clientId, String clientSecret, Environment environment) { 
+    this.environment = environment;
     
-    this.environment = Environment.PRODUCTION;
+		ClientCredentialsResourceDetails resourceDetails = new ClientCredentialsResourceDetails();
+		resourceDetails.setAccessTokenUri(this.environment.getAccessTokenUrl() + "/token");
+		resourceDetails.setClientId(clientId);
+		resourceDetails.setClientSecret(clientSecret);
+    
+    this.restTemplate = new OAuth2RestTemplate(resourceDetails);
   }
 
   public Product createLink(Product product) {
     // pegar access token
     restTemplate.getAccessToken();
 
-    String url = environment.getUrl();
+    String url = environment.getResourceUrl();
     ResponseEntity<Product> response =
       restTemplate.postForEntity(url + "/products/", product, Product.class);
 
     return response.getBody();
+  }
+
+  public Product getLink(String id) {
+    // pegar access token
+    restTemplate.getAccessToken();
+
+    String url = environment.getResourceUrl();
+    ResponseEntity<Product> response =
+      restTemplate.getForEntity(url + "/products/" + id, Product.class);
+
+    return response.getBody();
+  }
+
+  public void updateLink(String id, Product product) {
+    // pegar access token
+    restTemplate.getAccessToken();
+
+    String url = environment.getResourceUrl();
+    restTemplate.put(url + "/products/" + id, product);
+  }
+
+  public void deleteLink(String id) {
+    // pegar access token
+    restTemplate.getAccessToken();
+
+    String url = environment.getResourceUrl();
+    restTemplate.delete(url + "/products/" + id);
   }
   
 }
