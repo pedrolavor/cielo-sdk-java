0588f5137d26e1a9a3e7502a9f573f5c25a5498c
Pedro Lavôr <thiagolavor33@gmail.com>
2020-06-27 11:12:13 -0400

    ajuste diretorios e metodos dos models

--- src/main/java/cielo/lio/sdk/superlink/Environment.java
+++ src/main/java/cielo/lio/sdk/superlink/Environment.java
@@ -2,25 +2,13 @@ package cielo.lio.sdk.superlink;
 
 import lombok.Getter;
 
-/**
- * API Environment URLs
- * <p/>
- * <li>{@link #PRODUCTION}</li>
- * <li>{@link #SANDBOX}</li>
- */
 @Getter
 public enum Environment {
-	/**
-	 * Environment.PRODUCTION
-	 */
+
 	PRODUCTION(
 		"https://cieloecommerce.cielo.com.br/api/public/v2",
 		"https://cieloecommerce.cielo.com.br/api/public/v1"
 	),
-
-	/**
-	 * Environment.SANDBOX
-	 */
 	SANDBOX(
 		"https://cieloecommerce.cielo.com.br/sandbox/api/public/v2",
 		"https://cieloecommerce.cielo.com.br/sandbox/api/public/v1"
